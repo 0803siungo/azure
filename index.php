e0f278f4560b79e633bbb203818f8fa7caf0852c
0803siungo <123526618+0803siungo@users.noreply.github.com>
2023-07-31 12:36:31 +0700

    edittest

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-
-echo "Hello Sayang!!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gapura88</title>
+</head>
+<body>
+    <p>gapura-gacor</p>
+</body>
+</html>
